9acc426a5d2e64a0935ae5c2789e9adfa179539d
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-12-15 01:02:07 +0100

    Typed state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface Person {
+  id: number;
+  email: string;
+  first_name: string;
+  avatar: string;
+}
 
 function App() {
+  const [persons, setPersons] = useState<Person[]>([]);
+  useEffect(() => {
+    const fetchImages = async () => {
+      const data = (await axios.get("https://reqres.in/api/users/")).data
+        .data as Person[];
+      data.map((el) => el.avatar);
+      setPersons(data);
+    };
+    fetchImages();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {persons.map((person) => (
+        <React.Fragment key={person.id}>
+          <p>{person.email}</p>
+          <img src={person.avatar} />
+        </React.Fragment>
+      ))}
     </div>
   );
 }
